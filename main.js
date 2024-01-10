679d3d768518792c5e72c721e1afcba760f4b3f7
HalliS2o <hallist@hey.com>
2024-01-10 21:00:09 +0000

    did some styling

--- main.js
+++ main.js
@@ -14,17 +14,46 @@ fetch("http://localhost:3000/audio")
     .then((dataArray) => {
         dataArray.forEach((data) => {
             console.log(data)
-            const audio = document.createElement("audio")
-            audio.controls = true
-            audio.autoplay = true
-            audio.src = data.url
+            const audio = new Audio(data.url)
 
             const name = document.createElement("h2")
             name.innerText = data.name.split(".").slice(0, -1).join(".")
 
             const player = document.createElement("div")
+            const playPauseButton = document.createElement("button")
+            playPauseButton.classList.add("playPauseButton")
+            playPauseButton.innerText = "Play"
+            playPauseButton.addEventListener("click", playAudio)
+
+            const volumeSlider = document.createElement("input")
+            volumeSlider.type = "range"
+            volumeSlider.min = "0"
+            volumeSlider.max = "1"
+            volumeSlider.step = "0.01"
+            volumeSlider.value = "1"
+            volumeSlider.addEventListener("input", () => {
+                audio.volume = volumeSlider.value
+            })
+
+            volumeSlider.classList.add("volumeSlider")
+
+            function playAudio() {
+                audio.play()
+                playPauseButton.innerText = "Pause"
+                playPauseButton.removeEventListener("click", playAudio)
+                playPauseButton.addEventListener("click", pauseAudio)
+            }
+
+            function pauseAudio() {
+                audio.pause()
+                playPauseButton.innerText = "Play"
+                playPauseButton.removeEventListener("click", pauseAudio)
+                playPauseButton.addEventListener("click", playAudio)
+            }
+
             player.appendChild(name)
-            player.appendChild(audio)
+            player.appendChild(playPauseButton)
+            player.appendChild(volumeSlider) // Add the volume slider to the player
             player.classList.add("player")
 
             audios.appendChild(player)
